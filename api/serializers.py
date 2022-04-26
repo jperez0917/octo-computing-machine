39927f5dd1aeae44cbc45f779a85421cd72d9abd
Bruce Stull <bruce.stull@gmail.com>
2022-04-25 23:20:32 -0400

    Anonymous user has access to Link ListView.

--- api/serializers.py
+++ api/serializers.py
@@ -8,26 +8,47 @@ from links.models import Link
 class NestedLinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Link
-        fields = ('id', 'url_label', 'url', 'public')
+        fields = (
+            'id',
+            'url_label',
+            'url',
+            'notes',
+            'public',
+            )
 
 
 class NestedUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username')
+        fields = (
+            'id',
+            'username',
+            )
 
 
 class LinkSerializer(serializers.ModelSerializer):
     owner_detail = NestedUserSerializer(read_only=True, source='owner')
     class Meta:
         model = Link
-        fields = ('id', 'url_label', 'url', 'public', 'owner', 'owner_detail')
+        fields = (
+            'id',
+            'url_label',
+            'url',
+            'notes',
+            'public',
+            'owner',
+            'owner_detail',
+            )
 
 
 class UserSerializer(serializers.ModelSerializer):
     links_detail = NestedLinkSerializer(read_only=True, many=True, source='links')
     class Meta:
         model = get_user_model()
-        fields = ('id', 'username', 'links_detail')
+        fields = (
+            'id',
+            'username',
+            'links_detail',
+            )
 
 
