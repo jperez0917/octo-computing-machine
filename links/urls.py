a72425f87340b1b4ed609fcfed640438982555c1
Bruce Stull <bruce.stull@gmail.com>
2022-04-27 00:32:19 -0400

    Users can now delete Links if they are 'authenticated' and also the 'owner'.

--- links/urls.py
+++ links/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from links.views import LinkListView, LinkDetailView
-from links.views import LinkCreateView, LinkEditView
+from links.views import LinkCreateView, LinkEditView, LinkDeleteView
 
 app_name = 'links'
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('create/', LinkCreateView.as_view(), name='create'),
     path('<int:pk>/', LinkDetailView.as_view(), name='detail'),
     path('<int:pk>/edit/', LinkEditView.as_view(), name='edit'),
+    path('<int:pk>/delete/', LinkDeleteView.as_view(), name='delete'),
 ]
--- links/views.py
+++ links/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.mixins import UserPassesTestMixin
+from django.urls import reverse_lazy
 
 from links.models import Link
 
@@ -34,3 +35,13 @@ class LinkEditView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     def test_func(self):
         link = self.get_object()
         return self.request.user == link.owner
+
+
+class LinkDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Link
+    template_name = 'links\link_delete.html'
+    success_url = reverse_lazy('links:list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.owner
