bc982a610941d8c6bd6087ac91a73277597d8d36
Bruce Stull <bruce.stull@gmail.com>
2022-04-28 00:13:37 -0400

    Users can now view user profiles which list user Links.

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,5 @@ from accounts.views import SignUpView, UserProfileView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('<str:username>/', UserProfileView.as_view(), name='profile'),
+    path('profile/<str:username>/', UserProfileView.as_view(), name='profile'),
 ]
