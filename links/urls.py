9069dda326188f8f4214d844d49316b8c80b5b54
Bruce Stull <bruce.stull@gmail.com>
2022-04-30 21:34:06 -0400

    The general Link List view has been disabled.

--- links/urls.py
+++ links/urls.py
@@ -5,7 +5,7 @@ from links import views
 app_name = 'links'
 urlpatterns = [
     path('', views.PublicLinkListView.as_view(), name='public'),
-    path('list', views.LinkListView.as_view(), name='list'),
+    # path('list', views.LinkListView.as_view(), name='list'),
     path('profile/', views.LinkUserProfileView.as_view(), name='profile'),
     path('create/', views.LinkCreateView.as_view(), name='create'),
     path('<int:pk>/', views.LinkDetailView.as_view(), name='detail'),
