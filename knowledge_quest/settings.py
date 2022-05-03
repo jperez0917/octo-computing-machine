0e948b3621caa26ff6d8cd16f3632a96a0b87a24
Bruce Stull <bruce.stull@gmail.com>
2022-05-02 22:53:56 -0400

    Added some Django built-ins for testing and understanding.

--- knowledge_quest/settings.py
+++ knowledge_quest/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django.contrib.humanize',
+
     'django.contrib.admindocs',
 
     'accounts.apps.AccountsConfig',
